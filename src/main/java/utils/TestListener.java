2e7c81e8f249792cd9d0907429d29d357dedc7f1
Malevaniy Anton <goodrazer111@gmail.com>
2026-06-30 16:26:03 +0400

    1) Обновил базовую спеку 2) Переделал логин адаптер см учетом маскирования критичных данных как в логере, так и в аллюре. 3) +1 АТ

---
+++ src/main/java/utils/TestListener.java
@@ -0,0 +1,40 @@
+package utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import java.time.Duration;
+
+public class TestListener implements ITestListener {
+
+    private static final Logger log = LogManager.getLogger(TestListener.class);
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        log.info("==================================== STARTING TEST {} ======================================",
+                iTestResult.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        log.info("============================= FINISHED TEST {} Duration: {} ================================",
+                iTestResult.getName(), getExecutionTime(iTestResult));
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        log.info("============================ FAILED TEST {} Duration: {} ===================================",
+                iTestResult.getName(), getExecutionTime(iTestResult));
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        log.info("=================================== SKIPPING TEST {} =======================================",
+                iTestResult.getName());
+    }
+
+    private long getExecutionTime(ITestResult iTestResult) {
+        return Duration.ofMillis(iTestResult.getEndMillis() - iTestResult.getStartMillis()).toSeconds();
+    }
+}
--- src/test/java/api/tests/LoginApiTest.java
+++ src/test/java/api/tests/LoginApiTest.java
@@ -3,13 +3,15 @@ package api.tests;
 import api.adapters.BaseAdapter;
 import api.adapters.LoginAdapter;
 import io.qameta.allure.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginApiTest extends BaseAdapter {
 
     private final LoginAdapter loginAdapter = new LoginAdapter();
 
-    @Test(testName = "API.01.01 Авторизация пользователя с валидными данными (JSON)",
+    @Test(testName = "API.01.01. Авторизация пользователя с валидными данными (JSON)",
             description = "Авторизация пользователя с валидными данными (JSON) POST/login",
             priority = 1,
             groups = {"Positive", "Regression", "Smoke"},
@@ -24,10 +26,30 @@ public class LoginApiTest extends BaseAdapter {
     @Issue("BugLink")
     @Flaky
     @Owner("Malevaniy Anton")
-    public void checkingApiUserAuthorizationWithValidData() {
-        loginAdapter.authorizationWithValidCredentialsApi(validEmail, validPassword)
-                .then()
-                .log().all()
-                .spec(BaseAdapter.checkStatusCode(202));
+    public void checkingApiUserAuthorizationWithValidDataAPI() {
+        Response response = loginAdapter.authorizationWithValidCredentialsApi(validEmail, validPassword);
+        Assert.assertEquals(response.getStatusCode(), 202,
+                "Ошибка!!! Ожидался статус-код: 202, но сервер вернул: " + response.getStatusCode());
+    }
+
+    @Test(testName = "API.01.02. Проверка получения access_token в ответе POST/login",
+            description = "Проверка получения access_token в ответе POST/login",
+            priority = 1,
+            groups = {"Positive", "Regression", "Smoke"},
+            enabled = true)
+    @Description("Проверка получения access_token в ответе POST/login")
+    @Epic("Epic01_Авторизация")
+    @Feature("Проверка получения access_token")
+    @Story("Проверка получения access_token в ответе POST/login")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "docs.google", name = "Чек-лист PFLB")
+    @TmsLink("TestCaseLink")
+    @Issue("BugLink")
+    @Flaky
+    @Owner("Malevaniy Anton")
+    public void checkingWhetherAnAccessTokenInTheLoginResponseAPI() {
+        Response loginResponse = loginAdapter.authorizationWithValidCredentialsApi(validEmail, validPassword);
+        String accessToken = loginAdapter.obtainingATokenAPI(loginResponse);
+        Assert.assertFalse(accessToken.trim().isEmpty(), "Ошибка!!! Полученный токен пустой!");
     }
 }
